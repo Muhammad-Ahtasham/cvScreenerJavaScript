beb5ed3eeb7c065060ff16fc629e9a70181118d2
ATIII <ahtashamahsan988@gmail.com>
2022-07-27 01:11:54 +0500

    generated random data manually and pulled the random images using randomuser.me api

---
+++ index.js
@@ -0,0 +1,43 @@
+console.log('screening cvs');
+const data = [
+    {
+        name: 'ali', 
+        age: 16,
+        city: 'abc',
+        languages: 'python',
+        frameWork: 'Django',
+        image: 'https://randomuser.me/api/portraits/men/75.jpg'
+    },
+    {
+        name: 'ahmad', 
+        age: 16,
+        city: 'cde',
+        languages: 'cpp',
+        frameWork: 'Django',
+        image: 'https://randomuser.me/api/portraits/men/71.jpg'
+    },
+    {
+        name: 'ahsan', 
+        age: 26,
+        city: 'dfg',
+        languages: 'python',
+        frameWork: 'flask',
+        image: 'https://randomuser.me/api/portraits/men/70.jpg'
+    },
+    {
+        name: 'mehwish', 
+        age: 20,
+        city: 'fffs',
+        languages: 'python',
+        frameWork: 'cv2',
+        image: 'https://randomuser.me/api/portraits/women/75.jpg'
+    },
+    {
+        name: 'rida', 
+        age: 26,
+        city: 'abcqw',
+        languages: 'python',
+        frameWork: 'tkinter',
+        image: 'https://randomuser.me/api/portraits/women/51.jpg'
+    }
+]
